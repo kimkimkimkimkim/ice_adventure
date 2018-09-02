8e6022e47b46c9b5a5183d772d93ef461c9ba267
kimkimkimkimkim <kim1219sangwoo@yahoo.co.jp>
2018-09-03 05:18:33 +0900

    【機能追加】ゲームの大元の機能実装
    ・和尚本のChaptar5の部分まで終わらせた

---
+++ IceAdventure/Assets/Scripts/GameScene/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	//オブジェクト参照
+	public GameObject textGameOver; //ゲームオーバーテキスト
+	public GameObject textClear; //クリアテキスト
+
+	public enum GAME_MODE //ゲーム状態定義
+	{
+		PLAY, //プレイ中
+		CLEAR, //クリア
+	};
+
+	public GAME_MODE gameMode = GAME_MODE.PLAY; //ゲーム状態
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void GameOver(){
+		textGameOver.SetActive (true);
+	}
+
+	//ゲームクリア処理
+	public void GameClear(){
+		gameMode = GAME_MODE.CLEAR;
+		textClear.SetActive (true);
+	}
+}
--- IceAdventure/Assets/Scripts/GameScene/PlayerManager.cs
+++ IceAdventure/Assets/Scripts/GameScene/PlayerManager.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
 
+	//オブジェクト参照
+	public GameObject gameManager; //ゲームマネージャー
+
+	//グローバル変数
+	public LayerMask blockLayer; //ブロックレイヤー
+
 	//メンバ変数
 	private Rigidbody2D rbody; //プレイヤー制御用のrigidbody2d
 	private Vector3 nowPos; //プレイヤーの現在の位置
-	private float speed = 15; //プレイヤーの移動速度
+	private const float MOVE_SPEED = 3; //移動速度固定値
+	private float moveSpeed; //プレイヤーの移動速度
 	private float jumpPower = 400; //ジャンプの力
+	private bool goJump = false; //ジャンプしたかどうか
+	private bool canJump = false; //ブロックに設置しているかどうか
+
+	public enum MOVE_DIR //移動方向定義
+	{
+		STOP,
+		LEFT,
+		RIGHT,
+	};
+
+	private MOVE_DIR moveDirection = MOVE_DIR.STOP; //移動方向
 
 	// Use this for initialization
 	void Start () {
@@ -17,22 +35,93 @@ public class PlayerManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		nowPos = GetComponent<RectTransform> ().localPosition;
+
+		canJump = Physics2D.Linecast (transform.position - (transform.right * 0.1f),
+			transform.position - (transform.up * 0.1f), blockLayer) ||
+		Physics2D.Linecast (transform.position + (transform.right * 0.1f),
+			transform.position - (transform.up * 0.1f), blockLayer);
+
+		//何も押してないとき
+		moveDirection = MOVE_DIR.STOP;
 
 		//右方向キー
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			GetComponent<RectTransform> ().localPosition = new Vector3 (nowPos.x + speed,nowPos.y,nowPos.z);
+			moveDirection = MOVE_DIR.RIGHT;
 		}
 
 		//左方向キー
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			GetComponent<RectTransform> ().localPosition = new Vector3 (nowPos.x - speed,nowPos.y,nowPos.z);
+			moveDirection = MOVE_DIR.LEFT;
 		}
 
 		//上方向キー
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			rbody.AddForce (Vector2.up * jumpPower);
+			if (canJump) {
+				goJump = true;
+			}
 		}
 
+	}
+
+	void FixedUpdate(){
+		//移動方向で処理を分岐
+		switch (moveDirection) {
+		case MOVE_DIR.STOP: //停止
+			moveSpeed = 0;
+			break;
+		case MOVE_DIR.LEFT: //左に移動
+			moveSpeed = MOVE_SPEED * -1;
+			transform.localScale = new Vector2 (-0.15f, 0.15f);
+			break;
+		case MOVE_DIR.RIGHT: //右に移動
+			moveSpeed = MOVE_SPEED;
+			transform.localScale = new Vector2 (0.15f, 0.15f);
+			break;
+		}
+
+		rbody.velocity = new Vector2 (moveSpeed, rbody.velocity.y);
+
+		//ジャンプ処理
+		if (goJump) {
+			rbody.AddForce (Vector2.up * jumpPower);
+			goJump = false;
+		}
+	}
+
+
+
+	void OnCollisionStay2D(Collision2D col){
+		canJump = true;
+	}
+
+	void OnCollisionExit2D(Collision2D col){
+		canJump = false;
+	}
+
+	//衝突処理
+	void OnTriggerEnter2D(Collider2D col){
+
+		//プレイ中でなければ衝突判定は行わない
+		if (gameManager.GetComponent<GameManager> ().gameMode != GameManager.GAME_MODE.PLAY) {
+			return;
+		}
+
+		if(col.gameObject.tag == "Trap"){
+			gameManager.GetComponent<GameManager> ().GameOver ();
+			DestroyPlayer ();
+		}
+
+		if (col.gameObject.tag == "Goal") {
+			gameManager.GetComponent<GameManager> ().GameClear ();
+		}
+
+		if (col.gameObject.tag == "Snow") {
+			col.gameObject.GetComponent<SnowManager> ().GetSnow ();
+		}
+	}
+
+	//プレイヤーオブジェクト削除処理
+	void DestroyPlayer(){
+		Destroy (this.gameObject);
 	}
 }
